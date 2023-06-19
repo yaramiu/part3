2935c30843442f8028fa0e33c1ca86801ff6a76e
Brian Liao <yaramiu@gmail.com>
2023-06-19 13:16:46 -0400

    Allow new entries through HTTP POST requests

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+
 let persons = [
   {
     id: 1,
@@ -52,6 +54,26 @@ app.delete("/api/persons/:id", (request, response) => {
   persons = persons.filter((person) => person.id !== id);
 
   response.status(204).end();
+});
+
+app.post("/api/persons", (request, response) => {
+  const body = request.body;
+
+  if (!body.name) {
+    return response.status(400).json({ error: "name missing" });
+  } else if (!body.number) {
+    return response.status(400).json({ error: "number missing" });
+  }
+
+  const person = {
+    id: Math.floor(Math.random() * Number.MAX_SAFE_INTEGER),
+    name: body.name,
+    number: body.number,
+  };
+
+  persons = persons.concat(person);
+
+  response.json(person);
 });
 
 const PORT = 3001;
