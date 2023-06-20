f158015f10aeae9ed08ea7427e21aa40b6719586
Brian Liao <yaramiu@gmail.com>
2023-06-20 15:11:03 -0400

    Add production frontend build to app

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static("build"));
 
 const createPersonToken = (object) => {
   return morgan.token("persons", function (request, response) {
