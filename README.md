1b6d9c140efa05f751c0f053f2c0647b7a69af41
Brian Liao <yaramiu@gmail.com>
2023-06-20 14:21:45 -0400

    Deploy the backend

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # part3
+
+https://spring-meadow-6832.fly.dev/api/persons/
--- index.js
+++ index.js
@@ -114,7 +114,7 @@ app.post("/api/persons", (request, response) => {
   response.json(person);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
 });
