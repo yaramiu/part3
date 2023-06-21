ec275eb0a2a9fb37cc85c48d875fb61cb4bfe2fb
Brian Liao <yaramiu@gmail.com>
2023-06-21 12:52:17 -0400

    Create CLI for adding and getting objects from DB

---
+++ mongo.js
@@ -0,0 +1,47 @@
+const mongoose = require("mongoose");
+
+async function saveOrGetDataFromMongoDB() {
+  if (process.argv.length < 3) {
+    console.log("node mongo.js <password> [name] [number]");
+  }
+
+  const password = process.argv[2];
+
+  const uri = `mongodb+srv://fullstack:${password}@cluster0.8oocm9l.mongodb.net/phonebook?retryWrites=true&w=majority`;
+
+  mongoose.set("strictQuery", false);
+  mongoose.connect(uri);
+
+  const personSchema = new mongoose.Schema({
+    name: String,
+    number: String,
+  });
+
+  const Person = mongoose.model("Person", personSchema);
+
+  if (process.argv.length === 3) {
+    try {
+      const result = await Person.find({});
+      console.log(result);
+    } catch (error) {
+      console.log(error);
+    }
+  } else {
+    const person = new Person({
+      name: process.argv[3],
+      number: process.argv[4],
+    });
+
+    try {
+      const result = await person.save();
+      console.log(result);
+      console.log(`added ${person.name} number ${person.number} to phonebook`);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  mongoose.connection.close();
+}
+
+saveOrGetDataFromMongoDB();
